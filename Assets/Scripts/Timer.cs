0740b7890963abc6538d523fccebdd5ddbc875b9
Anatoli Zournatzi <82726455+AnatoliZournatzi@users.noreply.github.com>
2022-06-08 21:33:15 +0300

    Timer and Game Over Scene connected

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -8,7 +8,7 @@ using UnityEngine.UI;
 public class Timer : MonoBehaviour
 {
     public string LevelToLoad;
-    int countDownStartValue = 479; //initialized the variable
+    int countDownStartValue = 5; //initialized the variable
     public Text timerUI;
 
     //Use this for initialization

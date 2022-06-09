01593b95759557e717e66d56443a5a5e03d93ed8
Marianna Bosiaka <57630657+MariannaBosiaka@users.noreply.github.com>
2022-06-09 17:37:18 +0300

    final commit

--- Assets/Scripts/SceneChanges/ThirdSceneChange.cs
+++ Assets/Scripts/SceneChanges/ThirdSceneChange.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-//this is a script that is going to load the main menu scene after the credits scene
+//this is a script that is going to exit the game after the credits - after the player has completed the chapter 1 of the game
 
 //!!!this script is only going to work on PLAYMODE NOT on the timeline preview!!!
 public class ThirdSceneChange : MonoBehaviour
 {
     private void OnEnable()
     {
-        SceneManager.LoadScene("MenuScene", LoadSceneMode.Single);
+        Application.Quit();
     }
 }
